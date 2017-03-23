2fc21c56d0fdacbab2a2dec70038bc439b776ed1
adamhrz <adam.howitt@raizlabs.com>
2017-03-23 16:37:41 -0400

    Addressed PR feedback: creating a testable method for page name resolution, removing sample ignore and override lists and fixing whitespace issue

--- PRODUCTNAME/app/PRODUCTNAME/Application/AppLifecycle/AnalyticsConfiguration.swift
+++ PRODUCTNAME/app/PRODUCTNAME/Application/AppLifecycle/AnalyticsConfiguration.swift
@@ -14,15 +14,10 @@ struct AnalyticsConfiguration: AppLifecycle, PageNameConfiguration {
     // By default page names are the VC class name minus the suffix "ViewController" converted from camel case to title case. Adding a class to this list will use the provided string for that view controller.
     // e.g. ObjectIdentifier(SigninViewController.self): "Sign in",
     static let nameOverrides: [ObjectIdentifier : String] = [:]
-    // static let nameOverrides: [ObjectIdentifier : String] = [
-    //    ObjectIdentifier(SigninViewController.self): "Sign in",
-    // ]
 
     // Add any ViewControllers that you don't want to see in Analytics to the ignoreList
     // e.g. HomeTabBarViewController isn't really a screen to be tracked
-    static let ignoreList: [ObjectIdentifier] = [
-        ObjectIdentifier(UINavigationController.self),
-    ]
+    static let ignoreList: [ObjectIdentifier] = []
 
     var isEnabled: Bool {
         return true
