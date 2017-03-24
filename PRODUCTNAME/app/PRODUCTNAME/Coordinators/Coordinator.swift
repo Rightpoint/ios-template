6a69299fc9e15adca68563486d5ae15d16751c5c
nevillco <connor.neville@raizlabs.com>
2017-03-24 11:42:45 -0400

    Add folder/group structure for Coordinators, base AppCoordinator and Coordinator protocols

---
+++ PRODUCTNAME/app/PRODUCTNAME/Coordinators/Coordinator.swift
@@ -0,0 +1,15 @@
+//
+//  Coordinator.swift
+//  PRODUCTNAME
+//
+//  Created by Connor Neville on 3/24/17.
+//  Copyright © 2017 ORGANIZATION. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+
+    func start()
+
+}
