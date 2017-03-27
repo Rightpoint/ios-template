5747db792e1fffaaa538392ea145e232f877bda3
nevillco <connor.neville@raizlabs.com>
2017-03-27 09:41:49 -0400

    Add utility function for finding child coordinators, and empty SignInCoordinator

--- PRODUCTNAME/app/PRODUCTNAME/Coordinators/Coordinator.swift
+++ PRODUCTNAME/app/PRODUCTNAME/Coordinators/Coordinator.swift
@@ -15,3 +15,20 @@ protocol Coordinator {
     func start()
 
 }
+
+extension Coordinator {
+
+    /// Finds the first child coordinator of a given type.
+    ///
+    /// - Parameter ofType: the type of coordinator to search for.
+    /// - Returns: a tuple containing the index of the child,
+    /// and the child itself, if one was found.
+    func child<T: Coordinator>(ofType: T.Type) -> (index: Int, child: T)? {
+        if let index = childCoordinators.index(where: { $0 is T }),
+            let coordinator = childCoordinators.flatMap({ $0 as? T }).first {
+            return (index, coordinator)
+        }
+        return nil
+    }
+
+}
