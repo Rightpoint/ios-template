33bc2c384ca5d3262cc4c9c55d32b0efc5fe6d11
nevillco <connor.neville@raizlabs.com>
2017-06-08 08:58:10 -0400

    Use initialization block instead of computed var

--- PRODUCTNAME/app/PRODUCTNAME/Utilities/Formatters.swift
+++ PRODUCTNAME/app/PRODUCTNAME/Utilities/Formatters.swift
@@ -20,29 +20,16 @@ extension ISO8601DateFormatter: DateFormatable {}
 
 enum Formatters {
 
-    private static var formatters: [String: DateFormatable] = [:]
-
-    private struct Keys {
-        static let isoFormatter = "isoFormatter"
-    }
-
-    static var ISODateFormatter: DateFormatable {
-        if let formatter = formatters[Keys.isoFormatter] {
-            return formatter
-        }
-
+    static var ISODateFormatter: DateFormatable = {
         if #available(iOS 10.0, *) {
-            let isoformatter = ISO8601DateFormatter()
-            formatters[Keys.isoFormatter] = isoformatter
-            return isoformatter
+            return ISO8601DateFormatter()
         }
         else {
             let dateFormatter = DateFormatter()
             dateFormatter.locale = Locale(identifier: "en_US_POSIX")
             dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-            formatters[Keys.isoFormatter] = dateFormatter
             return dateFormatter
         }
-    }
+    }()
 
 }
