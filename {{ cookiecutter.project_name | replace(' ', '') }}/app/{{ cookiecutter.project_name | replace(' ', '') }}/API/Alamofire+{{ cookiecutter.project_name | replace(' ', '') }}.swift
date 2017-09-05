a93b26061de11ff5e61542a3fc0887022307df4d
Jay Clark <jason.clark@raizlabs.com>
2017-09-05 14:15:34 -0400

    Adds Query Params to APIEndpoint

--- {{ cookiecutter.project_name | replace(' ', '') }}/app/{{ cookiecutter.project_name | replace(' ', '') }}/API/Alamofire+{{ cookiecutter.project_name | replace(' ', '') }}.swift
+++ {{ cookiecutter.project_name | replace(' ', '') }}/app/{{ cookiecutter.project_name | replace(' ', '') }}/API/Alamofire+{{ cookiecutter.project_name | replace(' ', '') }}.swift
@@ -12,9 +12,19 @@ import Marshal
 extension SessionManager {
 
     func request<Endpoint: APIEndpoint>(_ baseURL: URL, endpoint: Endpoint) -> DataRequest {
-        guard let url = URL(string: endpoint.path, relativeTo: baseURL) else {
-            fatalError("Invalid Path Specification")
+        guard
+            let endpointURL = URL(string: endpoint.path, relativeTo: baseURL),
+            let url: URL = {
+                var urlComponents = URLComponents(url: endpointURL, resolvingAgainstBaseURL: true)
+                urlComponents?.queryItems = endpoint.queryParams?.flatMap { (name, value) in
+                    return URLQueryItem(name: name, value: value)
+                }
+                return urlComponents?.url
+            }()
+            else {
+                fatalError("Invalid Path Specification")
         }
+
         let request = self.request(
             url,
             method: endpoint.method,
