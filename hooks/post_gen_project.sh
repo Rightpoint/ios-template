01a798406aef606e60c18a53b3b665abd1c895ab
Brian King <brian.king@raizlabs.com>
2017-10-25 23:08:22 -0400

    Update notes

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -30,11 +30,13 @@ Congratulations! Please perform the following tasks:
 - Ensure your project name didn't include "-ios"
 - Enable Circle CI
   - Create a Status token in "Settings -> API Permissions" and replace ZZCIRCLE_PROJECT_STATUS_KEYZZ in README.md
+  - Under "Settings -> Advanced Settings", turn on "Only build pull requests"
 - Configure Github
   - Protect "master" and "develop". Set "develop" as the default branch
 - Ensure the Hockey Apps created have the correct teams
 - Review README.md architecture guidance
 - Make a PR against ios-template fixing any issues encountered
+  - Update fastlane / Cocoapods in the Gemfile
 EOF
 
 
