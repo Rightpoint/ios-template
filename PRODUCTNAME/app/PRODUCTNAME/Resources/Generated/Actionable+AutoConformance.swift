2eae147c39b4b7f2a69c49490865a090b3383167
Gary <gary.tokman@raizlabs.com>
2018-03-08 16:04:04 -0500

    Update generated auto conformance

--- PRODUCTNAME/app/PRODUCTNAME/Resources/Generated/Actionable+AutoConformance.swift
+++ PRODUCTNAME/app/PRODUCTNAME/Resources/Generated/Actionable+AutoConformance.swift
@@ -4,3 +4,67 @@
 
 
 //swiftlint:disable:previous vertical_whitespace
+
+// MARK: - AuthCoordinator
+protocol AuthCoordinatorDelegate: class {
+    func authCoordinator(_ coordinator: AuthCoordinator, didNotify action: AuthCoordinator.Action)
+}
+
+extension AuthCoordinator {
+
+    typealias ActionType = Action
+    typealias Delegate = AuthCoordinatorDelegate
+
+    func notify(_ action: ActionType) {
+        delegate?.authCoordinator(self, didNotify: action)
+    }
+
+}
+
+// MARK: - OnboardingCoordinator
+protocol OnboardingCoordinatorDelegate: class {
+    func onboardingCoordinator(_ coordinator: OnboardingCoordinator, didNotify action: OnboardingCoordinator.Action)
+}
+
+extension OnboardingCoordinator {
+
+    typealias ActionType = Action
+    typealias Delegate = OnboardingCoordinatorDelegate
+
+    func notify(_ action: ActionType) {
+        delegate?.onboardingCoordinator(self, didNotify: action)
+    }
+
+}
+
+// MARK: - OnboardingPageViewController
+protocol OnboardingPageViewControllerDelegate: class {
+    func onboardingPageViewController(_ vc: OnboardingPageViewController, didNotify action: OnboardingPageViewController.Action)
+}
+
+extension OnboardingPageViewController {
+
+    typealias ActionType = Action
+    typealias Delegate = OnboardingPageViewControllerDelegate
+
+    func notify(_ action: ActionType) {
+        delegate?.onboardingPageViewController(self, didNotify: action)
+    }
+
+}
+
+// MARK: - SignInCoordinator
+protocol SignInCoordinatorDelegate: class {
+    func signInCoordinator(_ coordinator: SignInCoordinator, didNotify action: SignInCoordinator.Action)
+}
+
+extension SignInCoordinator {
+
+    typealias ActionType = Action
+    typealias Delegate = SignInCoordinatorDelegate
+
+    func notify(_ action: ActionType) {
+        delegate?.signInCoordinator(self, didNotify: action)
+    }
+
+}
