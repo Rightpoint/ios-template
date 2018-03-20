3942da8f8045c010b9c6bd54d0c5e198cc5b46fe
Jay Clark <jason.clark@raizlabs.com>
2018-03-20 11:36:09 -0400

    SwiftGen Storyboards for prolonged splash screen purposes

---
+++ {{ cookiecutter.project_name | replace(' ', '') }}/app/{{ cookiecutter.project_name | replace(' ', '') }}/Resources/Generated/Storyboards.swift
@@ -0,0 +1,63 @@
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+// swiftlint:disable sorted_imports
+import Foundation
+import UIKit
+
+// swiftlint:disable file_length
+
+protocol StoryboardType {
+  static var storyboardName: String { get }
+}
+
+extension StoryboardType {
+  static var storyboard: UIStoryboard {
+    return UIStoryboard(name: self.storyboardName, bundle: Bundle(for: BundleToken.self))
+  }
+}
+
+struct SceneType<T: Any> {
+  let storyboard: StoryboardType.Type
+  let identifier: String
+
+  func instantiate() -> T {
+    guard let controller = storyboard.storyboard.instantiateViewController(withIdentifier: identifier) as? T else {
+      fatalError("ViewController '\(identifier)' is not of the expected class \(T.self).")
+    }
+    return controller
+  }
+}
+
+struct InitialSceneType<T: Any> {
+  let storyboard: StoryboardType.Type
+
+  func instantiate() -> T {
+    guard let controller = storyboard.storyboard.instantiateInitialViewController() as? T else {
+      fatalError("ViewController is not of the expected class \(T.self).")
+    }
+    return controller
+  }
+}
+
+protocol SegueType: RawRepresentable { }
+
+extension UIViewController {
+  func perform<S: SegueType>(segue: S, sender: Any? = nil) where S.RawValue == String {
+    performSegue(withIdentifier: segue.rawValue, sender: sender)
+  }
+}
+
+// swiftlint:disable explicit_type_interface identifier_name line_length type_body_length type_name
+enum StoryboardScene {
+  enum LaunchScreen: StoryboardType {
+    static let storyboardName = "LaunchScreen"
+
+    static let initialScene = InitialSceneType<UIViewController>(storyboard: LaunchScreen.self)
+  }
+}
+
+enum StoryboardSegue {
+}
+// swiftlint:enable explicit_type_interface identifier_name line_length type_body_length type_name
+
+private final class BundleToken {}
